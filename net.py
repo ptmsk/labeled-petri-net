853adf63360ebc5979d4d5e4899da6862ffc356d
ptmsk <huy.tranacc13042@hcmut.edu.vn>
2021-11-17 15:22:55 +0700

    Finish source and do some asm1 exercise

--- net.py
+++ net.py
@@ -1,5 +1,8 @@
+from os import sep
+from typing_extensions import Concatenate
+
 class Place:
-    def __init__(self, name, holding=0):
+    def __init__(self, name, holding=0, max_token=-1):
         """
         Place object in Petri Net
         :name: Name of place.
@@ -7,7 +10,14 @@ class Place:
         """
         self._name = name
         self._holding = holding
+        self._max_token = max_token
     
+    def receivable(self, tokens=1):
+        """
+        Check whether place can receive more tokens
+        tokens: number of tokens received
+        """
+        return (self._max_token == -1 or self._holding + tokens <= self._max_token)
 
 class ArcBase:
     def __init__(self, place, weight=1):
@@ -23,23 +33,30 @@ class ArcBase:
 class OutArc(ArcBase):
     def __init__(self, place, weight=1):
         """
-        Directed arc from transition to output place.
+        Directed arc from transition to output place.  
         :place: Output place.
         :weight: Number of tokens added to place.
         """
-        super().__init__(place, weight)
-    
+        super().__init__(place, weight)        
+
+    def enabled(self):
+        """
+        Check whether the outarc of the transition is enabled
+        """
+        return self._place.receivable()
+
     def produce(self):
         """
         Produce tokens in output places
         """
-        self._place._holding += self._weight
+        if self.enabled():
+            self._place._holding += self._weight
 
 
 class InArc(ArcBase):
     def __init__(self, place, weight=1):
         """
-         Directed arc from input place to transition.
+        Directed arc from input place to transition.
         :place: Input place.
         :weight: Number of tokens removed from place.
         """
@@ -47,7 +64,7 @@ class InArc(ArcBase):
 
     def enabled(self):
         """
-        Check if the trasition is enabled
+        Check if the inarc of the trasition is enabled
         """
         return self._place._holding >= self._weight
 
@@ -55,7 +72,8 @@ class InArc(ArcBase):
         """
         Consume tokens from input places
         """
-        self._place._holding -= self._weight
+        if self.enabled():
+            self._place._holding -= self._weight
 
 
 class Transition:
@@ -74,12 +92,19 @@ class Transition:
         self._inarcs = set(inarcs)
         self._outarcs = set(outarcs)
 
+    def fireable(self):
+        """"
+        Check whether the transition is fireable
+        """
+        return (all(inarc.enabled() for inarc in self._inarcs)
+                and any(outarc.enabled() for outarc in self._outarcs))
+
     def fire(self):
         """
         Apply firing rule
         """
         # Check if the trasition is enabled.
-        enabled = all(inarc.enabled() for inarc in self._inarcs)
+        enabled = self.fireable()
 
         if enabled:
             for arc in self._inarcs:
@@ -90,61 +115,168 @@ class Transition:
 
     
 class PetriNet:
-    def __init__(self, *transitions):
+    def __init__(self, transitions, places):
         """
         Petri Net object.
         :transition: The transitions (which include the places in arc).
+        :places: Places in petri net
         """
         self._transitions = transitions
+        self._places = places
+        self._n_transitions = len(transitions)
+    
+    def set_markings(self, markings):
+        for i in range(len(markings)):
+            self._places[i]._holding = markings[i]
 
-    def fsgenerate(self):
+    def run_concurrent(self):
         """
-        Firing sequence generator.
+        Fire all available transitions concurrently in the net until none left
+        Return all transition relations in the net
         """
-        transitions_name = (ts._name for ts in self._transitions)
-        print("Enter a firing sequence", tuple(["{0} for '{1}'".format(*i) for i in enumerate(transitions_name)]), ":")
-        fs = input()
-        # Ex: fs = "0 0 1 1 0 2 1"
-        return list(map(int, map(str.strip, fs.split())))
+        print("Initial marking: [", ", ".join(["{0}.{1}".format(p._holding, p._name) for p in self._places]), "]", sep="")
 
-    def run(self, pt):
-        """
-        Activate firing sequence.
-        :pt: list of places with tokens.
-        """
-        firing_sequence = petri_net.fsgenerate()
-        print("Using firing sequence:\n" + " => ".join([self._transitions[i]._name for i in firing_sequence]))
-        print("Initial marking: [", ", ".join(["{0}.{1}".format(p._holding, p._name) for p in pt]), "]")
+        markings_set = set()    # use to check whether loop contains in the net
+        markings_set.add(tuple(p._holding for p in self._places))
+        transition_relations = set()    
 
-        for i in firing_sequence:
-            if self._transitions[i].fire():
-                print("'{}' fired...".format(self._transitions[i]._name))
-                print("\t ===>    [", ", ".join(["{0}.{1}".format(p._holding, p._name) for p in pt]), "]")
-            else:
-                print("'{}' is not enabled...".format(self._transitions[i]._name))
+        while any(transition.fireable() for transition in self._transitions):
+            concurrent_trans = []   # all fireable transitions index
+
+            print("[", ", ".join(["{0}.{1}".format(p._holding, p._name) for p in self._places]), "]", end="  ", sep="")
+            
+            s1 = [p._holding for p in self._places]
+            s2 = []
+            for i in range(self._n_transitions):
+                if self._transitions[i].fireable():
+                    concurrent_trans.append(i)
+                    s2.append(self._transitions[i]._name)
+
+            # fire concurrently
+            for i in concurrent_trans:
+                self._transitions[i].fire()
+
+            print("[", ", ".join(["{0}".format(self._transitions[i]._name) for i in concurrent_trans]), ">", 
+                            "  [", ", ".join(["{0}.{1}".format(p._holding, p._name) for p in self._places]), "]", sep="")
+            
+            s3 = [p._holding for p in self._places]
+            transition_relations.add(tuple([tuple(s1), tuple(s2), tuple(s3)]))
+            
+            # check whether contains loop in the nets
+            old_size = markings_set.__len__()
+            markings_set.add(tuple(s3))
+            if old_size == markings_set.__len__():
+                print("Loop detected! Terminate firing!")
+                print("\nEnd firing: [", ", ".join(["{0}.{1}".format(p._holding, p._name) for p in self._places]), "]", sep="")
+                return transition_relations
+            
+        print("\nEnd firing: [", ", ".join(["{0}.{1}".format(p._holding, p._name) for p in self._places]), "]", sep="")
+        return transition_relations
+
+    def run_sequent_rec(self, transition_indices, trans_relations):
+        """ 
+        Fire all available transitions sequentially in the net until none left
+        transition_indices: all fireable transitions
+        trans_relations: all transition relations in the net
+        """
+        for idx in transition_indices:
+            reserve_markings = [p._holding for p in self._places]   # reserve markings for recursion
+            
+            s1 = [p._holding for p in self._places]
+            s2 = [self._transitions[idx]._name] 
+
+            print("[", ", ".join(["{0}.{1}".format(p._holding, p._name) for p in self._places]), "]", end="  ", sep="")
+            self._transitions[idx].fire()
+            print("[", self._transitions[idx]._name, ">", 
+                    "  [", ", ".join(["{0}.{1}".format(p._holding, p._name) for p in self._places]), "]", sep="")
+
+            s3 = [p._holding for p in self._places]
+
+            old_size = trans_relations.__len__()
+            trans_relations.add(tuple([tuple(s1), tuple(s2), tuple(s3)]))
+            if old_size == trans_relations.__len__():
+                return
+
+            fireable_trans = []
+            for i in range(self._n_transitions):
+                if self._transitions[i].fireable():
+                    fireable_trans.append(i)
+            
+            for i in fireable_trans:
+                self.run_sequent_rec([i], trans_relations)
+
+            # restore markings
+            self.set_markings(reserve_markings)
+            
+        if not(any(transition.fireable() for transition in self._transitions)):
+            return
         
-        print("\nEnd firing: [", ", ".join(["{0}.{1}".format(p._holding, p._name) for p in pt]), "]")
-
-    def run_debug(self, pt):
+    def run_sequent(self):
+        """ 
+        Fire all available transitions sequentially in the net until none left
+        Return all transition relations in the net
         """
-        Apply firing sequence with debug mode.
-        :pt: list of places with tokens.
-        """
-        print("Initial marking: [", ", ".join(["{0}.{1}".format(p._holding, p._name) for p in pt]), "]")
-        transitions_name = (ts._name for ts in self._transitions)
-        choose_transition = "Choose a firing transition (" + ", ".join(["{0} for '{1}'".format(*i) for i in enumerate(transitions_name)]) +  ", -1 to finish firing): "
-        i = int(input(choose_transition).strip())
+        fireable_trans = []
+        for i in range(self._n_transitions):
+            if self._transitions[i].fireable():
+                fireable_trans.append(i)
+        trans_relations = set()
+        self.run_sequent_rec(fireable_trans, trans_relations)
+        return trans_relations
 
-        while i != -1:
-            if self._transitions[i].fire():
-                print("'{}' fired...".format(self._transitions[i]._name))
-                print("\t ===>    [", ", ".join(["{0}.{1}".format(p._holding, p._name) for p in pt]), "]")
-            else:
-                print("'{}' is not enabled...".format(self._transitions[i]._name))
-            i = int(input(choose_transition).strip())
+    def result_firing_one(self):
+        if self._n_transitions == 0:
+            print ("Empty petri net!")
+            return
+        print("Initial marking: [", ", ".join(["{0}.{1}".format(p._holding, p._name) for p in self._places]), "]", sep="")
+        initial_marking = [place._holding for place in self._places]
+        check_exist_marking = False
+        for ts in self._transitions:
+            enabled = ts.fire()
+            if enabled:
+                print("(N, M0) [{0}> (N, [{1}])".format(ts._name, ", ".join(["{0}.{1}".format(p._holding, p._name) for p in self._places])))
+                check_exist_marking = True
+                self.set_markings(initial_marking)
+        if check_exist_marking == False:
+            print("No reachable marking exists!")
 
-        print("\nEnd firing: [", ", ".join(["{0}.{1}".format(p._holding, p._name) for p in pt]), "]")
+def testPNA():
+    place_names = ["wait", "inside", "done"]
+    print("please enter the number of tokens in place 'wait', 'inside', 'done', respectively: ")
+    place_holding = [int(input()) for _ in range(3)]
+    place = list(zip(place_names, place_holding))
 
+    obj_place = [Place(*p) for p in place]
+
+    start = Transition("start", InArc(obj_place[0]), OutArc(obj_place[1]))
+    change = Transition("change", InArc(obj_place[1]), OutArc(obj_place[2]))
+    
+    petri_net = PetriNet([start, change], obj_place) # or PetriNet([start, change])
+    petri_net.run_concurrent()
+    petri_net.set_markings([2, 1, 1])
+    trans_relation = petri_net.run_sequent()
+    for trans in trans_relation:
+        print(trans)
+
+def testPS():
+    place_names = ['free', 'busy', 'docu']
+    print("please enter the number of tokens in place 'free', 'busy', 'docu', respectively: ")
+    place_holding = [int(input()) for _ in range(3)]
+    place = list(zip(place_names, place_holding))
+
+    obj_place = [Place(*p) for p in place]
+
+    start = Transition('start', InArc(obj_place[0]), OutArc(obj_place[1]))
+    change = Transition("change", InArc(obj_place[1]), OutArc(obj_place[2]))
+    end = Transition("end", InArc(obj_place[2]), OutArc(obj_place[0]))
+
+    petri_net = PetriNet([start, change, end], obj_place)
+    petri_net.run_concurrent()
+    petri_net.set_markings([1, 0, 0])
+    trans_relation = petri_net.run_sequent()
+    for trans in trans_relation:
+        print(trans)
+        
 if __name__ == "__main__":
     place_names = ["wait", "inside", "done"]
     print("please enter the number of tokens in place 'wait', 'inside', 'done', respectively: ")
@@ -155,6 +287,7 @@ if __name__ == "__main__":
 
     start = Transition("start", InArc(obj_place[0]), OutArc(obj_place[1]))
     change = Transition("change", InArc(obj_place[1]), OutArc(obj_place[2]))
+    
+    petri_net = PetriNet([start, change], obj_place) # or PetriNet([start, change])
 
-    petri_net = PetriNet(start, change) # or PetriNet([start, change])
-    petri_net.run_debug(obj_place)
+    petri_net.result_firing_one()
